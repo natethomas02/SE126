f582ecfe980a2942301ada1ba1600342ec4626a7
natethomas02 <njthomas@email.neit.edu>
2024-01-20 18:09:20 -0500

    update lab

--- week2_lab/week2_lab.py
+++ week2_lab/week2_lab.py
@@ -1,25 +1,31 @@
 import csv
-total_records = 0
-records_processed = 0
 
+def process_rooms(filename):
+    records_processed = 0
+    rooms_over_limit = 0
 
-room = []
-maxpeople = []
-regpeople = []
-
-
-
-with open("week2_lab/lab2a.txt") as csvfile:
-
-    file = csv.reader(csvfile)
-
-    for rec in file:
-    
-        print(rec)
-
-
-        room.append(rec[0])
-        maxpeople.append(int(rec[1]))
-        regpeople.append(int(rec[2]))
-
+    with open(filename, 'r') as csvfile:
+        file = csv.reader(csvfile)
         
+        
+        next(file, None)
+
+        for row in file:
+            records_processed += 1
+
+            room_name, max_cap, current_registered = row
+            max_cap = int(max_cap)
+            current_registered = int(current_registered)
+
+            if current_registered > max_cap:
+                rooms_over_limit += 1
+                print(f"Room {room_name} is over the maximum limit. Notify {current_registered - max_cap} people.")
+
+    print(f"\nNumber of records processed: {records_processed}")
+    print(f"Number of rooms over limit: {rooms_over_limit}")
+
+
+filename = 'week2_lab/lab2a.txt'
+
+
+process_rooms(filename)
