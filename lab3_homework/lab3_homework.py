dc61cd4ece12d89d9fb1047380aebc46342993df
natethomas02 <njthomas@email.neit.edu>
2024-01-31 18:37:07 -0500

    finish lab

--- lab3_homework/lab3_homework.py
+++ lab3_homework/lab3_homework.py
@@ -1,6 +1,57 @@
+#Late Thomas
+#se126.10
+#1.31.2024
+#program prompt-
+#variable dictionary-
+#total_records = 0
+#under_age = 0
+#elig_notreg = 0
+#reg_novote = 0
+#reg_and_voted = 0
+#
+#
+#-------------------------------------------------------------------------
+#import csv file
 import csv
 
+#Assign variables and lists
+total_records = 0
+under_age = 0
+elig_notreg = 0
+reg_novote = 0
+reg_and_voted = 0
+age = []
+registered = []
+voted = []
 
-with open("lab3_homework\voters_202040.csv") as csvfile:
+#connect to file and read data
+with open("lab3_homework/voters_202040.csv") as csvfile:
+    file = csv.reader(csvfile)
 
-        file = csv.reader(csvfile)
+    for rec in file:
+        #store data from file fields to their lists and total_records counter
+        total_records += 1
+        age.append(int(rec[1]))
+        registered.append(rec[2])
+        voted.append(rec[3])
+#print total records recorded
+print(f"Total records processed: {total_records}")
+print("-----------------------------------------------")
+#for loop, set counters for appropriate variables
+for i in range(total_records):
+    if age[i] < 18:
+        under_age += 1
+    elif age[i] >= 18 and registered[i] == "N":
+        elig_notreg += 1
+    elif registered[i] == "Y" and voted[i] == "N":
+        reg_novote += 1
+    elif registered[i] == "Y" and voted[i] == "Y":
+        reg_and_voted += 1
+#print output
+print(f"\nIndividuals not eligible to register: {under_age}")
+print(f"\nIndividuals old enough to vote but have not registered: {elig_notreg}")
+print(f"\nIndividuals eligible to vote but did not vote: {reg_novote}")
+print(f"\nIndividuals voted: {reg_and_voted}")
+print("-----------------------------------------------")
+# print good bye message
+print("\n GOODBYE!")
--- thomas_w2_inclass.py
+++ thomas_w2_inclass.py
@@ -1,10 +1,44 @@
-#use this file to work on the in-class lab due friday january 19th
+import csv
+total_records = 0
+under_age = 0
+elig_notreg = 0
+reg_novote = 0
+reg_and_voted = 0
+age = []
+registered = []
+voted = []
 
-import csv 
 
-total_rooms = 0
-rooms_over = 0
 
-with open() as csvfile:
+with open("lab3_homework/voters_202040.csv") as csvfile:
 
-    file = csv.reader(csvfile)
+        file = csv.reader(csvfile)
+
+        for rec in file:
+                total_records += 1 
+                age.append(int(rec[1]))
+                registered.append(rec[2])
+                voted.append(rec[3])
+
+print(f"Total records processed: {total_records}")
+
+
+
+for i in range(total_records):
+        if age[i] < 18:
+                under_age += 1
+        elif age[i] >= 18 and registered[i] == "N":
+                elig_notreg += 1
+        elif registered[i] == "Y" and voted == "Y":
+                reg_novote += 1
+        elif registered[i] == "Y" and voted == "Y":
+                reg_and_voted += 1
+
+print(f"Individuals not eligible to register: {under_age}")
+
+print(f"Individuals old enough to vote but have not registered: {elig_notreg}")
+
+print(f"Individuals eligible to vote but did not vote: {reg_novote}")
+
+print(f"Individuals voted: {reg_and_voted}")
+
